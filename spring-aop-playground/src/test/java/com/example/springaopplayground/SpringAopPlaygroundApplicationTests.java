35ade5bfd9e2e305b3ee765af6dfc98aa19d025d
Wishoon <heonga2@gmail.com>
2022-09-25 16:52:32 +0900

    feat: spring playground 프로젝트 설정

---
+++ spring-aop-playground/src/test/java/com/example/springaopplayground/SpringAopPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaopplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
